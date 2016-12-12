ea9f21bd91ba03af175724470008050d5f63a154
Josep M Sobrepere <josepot@gmail.com>
2016-12-11 22:28:15 -0500

    9b solved

--- day9b.js
+++ day9b.js
@@ -1,27 +1,21 @@
-const R = require('ramda');
 const readInput = require('./utils/read-input.js');
 
-const repeatText = (text, nTimes) => {
-  let result = '';
-  for (let i = 0; i < nTimes; i++) result += text;
-  return result;
-}
-
-const processLine = (line, processedText = '', inheritedRepetitions = 1) => {
+const processLine = (line, processedAmount = 0, inheritedRepetitions = 1) => {
   const parts =
     line.match(/^([0-9]*?x?[0-9]*?\)?[A-Z]*)?\(([0-9]*x[0-9]*)\)(.*)$/);
 
-  if (!parts) return processedText + repeatText(line, inheritedRepetitions);
+  if (!parts) return processedAmount + (line.length * inheritedRepetitions);
 
   const [ charsToProcess, repetitions ] =
     parts[2].split('x').map(x => parseInt(x));
 
   const partToRepeat = parts[3].substr(0, charsToProcess);
-  const repeatedText = processLine(partToRepeat, '', inheritedRepetitions * repetitions);
+  const body = processLine(partToRepeat, 0, inheritedRepetitions * repetitions);
+  const head = parts[1] ? parts[1].length * inheritedRepetitions : 0;
 
   return processLine(
     parts[3].substr(charsToProcess),
-    processedText + (parts[1] || '') + repeatedText,
+    processedAmount + head + body,
     inheritedRepetitions
   );
 };
@@ -29,9 +23,5 @@ const processLine = (line, processedText = '', inheritedRepetitions = 1) => {
 readInput(9)
   .then(input => input.substr(0, input.length -1))
   .then(processLine)
-  .then(result => console.log(result.length))
-  .catch(e => {
-    console.log(e.message);
-    console.log(e.stack);
-  })
+  .then(result => console.log(result))
 ;
